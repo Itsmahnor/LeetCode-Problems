9cc3558e253313c53d0111b2c55e73fdba0d262c
Mahnoor Sarfraz <mahnoorsarfraz23@gmail.com>
2024-09-12 10:08:40 +0500

    Time: 6 ms (50.54%), Space: 15.7 MB (98.76%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxSum=0;
+        int buy=prices[0];
+        int profit=0;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i] < buy){
+                buy=prices[i];
+            }
+            else if(prices[i] - buy > profit){
+                profit=prices[i] - buy;
+            }
+            if(profit!=0){
+                maxSum+=profit;
+                profit=0;
+                buy=prices[i];
+            }
+
+        }
+        return maxSum;
+    }
+};
