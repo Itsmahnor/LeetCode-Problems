0889deacda8fac208f94b61878cd517e83f71ff2
Mahnoor Sarfraz <mahnoorsarfraz23@gmail.com>
2026-03-12 05:59:41 +0500

    Time: 0 ms (100%), Space: 9.7 MB (73.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-   
-bool isAnagram(string s, string t) {
-   if(s.size()!=t.size()){return false;}
-   unordered_map<char,int> m1;
-   unordered_map<char,int> m2;
-   for(auto i:s){
-    m1[i]++;
-   }
-     for(auto i:t){
-    m2[i]++;
-   }
-   for(auto i:s){
-    if(m1[i] != m2[i]){
-        return false;
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+       int arr[26] ={0};
+       int arr1[26] ={0};
+      
+        for(int i=0;i<s.length();i++){
+            int value = s[i] - 'a';
+            arr[value]++;
+        }
+         for(int i=0;i<t.length();i++){
+            int value = t[i] - 'a';
+            arr1[value]++;
+        }
+        bool results = false;
+      for(int i=0;i<=25;i++){
+         
+     
+        if(arr[i] == arr1[i]){
+            results=true;
+        }else{
+            return false;
+        }
+        }  
+
+        return results;   
     }
-   }
-   return true;
-}
 };
