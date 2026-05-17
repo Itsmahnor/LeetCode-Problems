5fb0a5ffab13e929fe4863133cd4e324a9a763d0
Mahnoor Sarfraz <mahnoorsarfraz23@gmail.com>
2026-05-17 12:04:14 +0500

    Time: 0 ms (100%), Space: 8.4 MB (31.12%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int i=0;
+
+        while(i<word.length()){
+            if(word[i] == ch){
+               reverse(word.begin(),word.begin()+i+1);
+               break;
+            }
+            i++;
+        }
+        return word;
+    }
+};
