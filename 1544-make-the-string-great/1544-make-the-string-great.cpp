263a4634a9936244166eeb5580384e8dfa05482e
Mahnoor Sarfraz <mahnoorsarfraz23@gmail.com>
2026-05-24 21:03:14 +0500

    Time: 0 ms (100%), Space: 8.8 MB (33.16%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     string makeGood(string s) {
-      string s2;
-      stack<char> s1;
-      for(char c:s){
-        if(!s1.empty() && tolower(c) == tolower(s1.top()) && c!=s1.top()){
+        stack<char> s1;
+
+        for (char c : s) {
+            if (!s1.empty() && abs(s1.top() - c) == 32) {
+                s1.pop();
+            } else {
+                s1.push(c);
+            }
+        }
+
+        string res;
+
+        while (!s1.empty()) {
+            res += s1.top();
             s1.pop();
         }
-        else{
-           s1.push(c);
-        }
-      }
-      while (!s1.empty()) {
-            s2 = s1.top() + s2;
-         
-            s1.pop();
-        }
-      return s2;
+
+        reverse(res.begin(), res.end());
+
+        return res;
     }
 };
