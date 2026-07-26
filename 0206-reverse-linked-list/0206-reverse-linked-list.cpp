c47d9f903c4c7d54b250491f154ab42305c86b86
Mahnoor Sarfraz <mahnoorsarfraz23@gmail.com>
2026-07-26 11:20:22 +0500

    Time: 0 ms (100%), Space: 13.5 MB (42.36%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,42 +11,58 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-    if(head == nullptr) {return head;}
-        ListNode* current = head;
-        int count = 0;
-        while(current != nullptr){
-            count++;
-            current = current -> next;
-        }
-        if(count == 1){return head;}
-       vector<int> arr(count);
+    // if(head == nullptr) {return head;}
+    //     ListNode* current = head;
+    //     int count = 0;
+    //     while(current != nullptr){
+    //         count++;
+    //         current = current -> next;
+    //     }
+    //     if(count == 1){return head;}
+    //    vector<int> arr(count);
     
-        current = head;
-        for(int i=0; i<count ; i++){
-            arr[i] = current -> val;
-            current = current -> next;
-        }
-        reverse(arr.begin(),arr.end());
-        ListNode* newhead = nullptr;
-        ListNode* newcurrent;
+    //     current = head;
+    //     for(int i=0; i<count ; i++){
+    //         arr[i] = current -> val;
+    //         current = current -> next;
+    //     }
+    //     reverse(arr.begin(),arr.end());
+    //     ListNode* newhead = nullptr;
+    //     ListNode* newcurrent;
 
-        for(int i=0; i<count; i++ ){
-        ListNode* newptr = new ListNode(arr[i]);
-        if(newhead == nullptr){
-            newhead = newptr;
-            newcurrent = newhead;
-            newhead -> next = newcurrent;
-        }else{
-            newcurrent->next = newptr;
-            newcurrent = newcurrent -> next;
-        }
-        }
-        return newhead;
+    //     for(int i=0; i<count; i++ ){
+    //     ListNode* newptr = new ListNode(arr[i]);
+    //     if(newhead == nullptr){
+    //         newhead = newptr;
+    //         newcurrent = newhead;
+    //         newhead -> next = newcurrent;
+    //     }else{
+    //         newcurrent->next = newptr;
+    //         newcurrent = newcurrent -> next;
+    //     }
+    //     }
+    //     return newhead;
 
 
         
 
 
 
+    // }
+    if(head == nullptr || head->next  == nullptr){return head;}
+    ListNode* prev;
+    ListNode* current;ListNode* next;
+    prev = nullptr;
+    current = head;
+    next = current -> next;
+    while(current != nullptr){
+        next = current -> next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    } 
+    return prev;
     }
+
+
 };
