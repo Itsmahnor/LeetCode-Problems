46334e71f14c02eb65e341357e377a2570151e9b
Mahnoor Sarfraz <mahnoorsarfraz23@gmail.com>
2026-08-05 18:10:18 +0500

    Time: 0 ms (100%), Space: 9.9 MB (78.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string a;
-        for(int i=0;i<s.length();i++){
-            if(isalnum(s[i])){
+        return check(s, 0, s.length() - 1);
+    }
 
-                a+=tolower(s[i]);
-            }
-        }
-        string rev = a;
-        reverse(rev.begin(),rev.end());
-        if(rev == a){
+    bool check(string &s, int left, int right) {
+
+        // Base case
+        if (left >= right)
             return true;
-        }else{
-            return false;
+
+        // Skip non-alphanumeric from left
+        if (!isalnum(s[left])) {
+            return check(s, left + 1, right);
         }
+
+        // Skip non-alphanumeric from right
+        if (!isalnum(s[right])) {
+            return check(s, left, right - 1);
+        }
+
+        // Compare lowercase characters
+        if (tolower(s[left]) != tolower(s[right]))
+            return false;
+
+        // Move both pointers
+        return check(s, left + 1, right - 1);
     }
 };
